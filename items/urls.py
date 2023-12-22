90f9a10690e013096bf24d63616b2cc833cb078c
narendra <naren.alp@gmail.com>
2023-12-22 16:07:16 +0530

    First Commit Second

--- items/urls.py
+++ items/urls.py
@@ -3,8 +3,4 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<int:item_code>/", views.detail, name="detail"),
-    path("<int:item_code>/price/", views.price, name="price"),
-    path("<int:item_code>/unit/", views.unit, name="unit"),
-
 ]
